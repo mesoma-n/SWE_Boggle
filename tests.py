21d75f36a13f625c1fcf6352e4588588436b025b
Mesoma Nwankwo <mesomachukwu.nwankw@bison.howard.edu>
2024-11-01 04:45:17 +0000

    test

--- tests.py
+++ tests.py
@@ -1,58 +1,62 @@
 import unittest
 import sys
-
-sys.path.append("/home/codio/workspace/") #have to tell the unittest the PATH to find boggle_solver.py and the Boggle Class
-
 from boggle_solver import Boggle
 
-class TestSuite_Alg_Scalability_Cases(unittest.TestCase):
+# Adding the path to locate boggle_solver.py
+sys.path.append("/home/codio/workspace")
 
-  # ADD 4x4, 5x5, 6x6, 7x7...13x13, and LARGER Dictionaries
-  def test_Normal_case_3x3(self):
-    grid = [["A", "B", "C"],["D", "E", "F"],["G", "H", "I"]]
-    dictionary = ["abc", "abdhi", "abi", "ef", "cfi", "dea"]
-    mygame = Boggle(grid, dictionary)
-    solution = mygame.getSolution()
-    solution = [x.upper() for x in solution]
-    expected = ["abc", "abdhi", "cfi", "dea"];
-    expected = [x.upper() for x in expected]
-		solution = sorted(solution)
-    expected = sorted(expected)
-    self.assertEqual(expected, solution)
 
-class TestSuite_Simple_Edge_Cases(unittest.TestCase):
-  #ADD MANY SIMPLE TEST CASES
-  def test_SquareGrid_case_1x1(self):
-    grid = [["A"]];
-    dictionary = ["a", "b", "c"]
-    mygame = Boggle(grid, dictionary)
-    solution = mygame.getSolution()
-    solution = [x.upper() for x in solution]
-    expected = []
-		solution = sorted(solution)
-    expected = sorted(expected)
-    self.assertEqual(expected, solution)
+class TestSuiteAlgScalabilityCases(unittest.TestCase):
 
-  def test_EmptyGrid_case_0x0(self):
-    grid = [[]];
-    dictionary = ["hello", "there", "general", "kenobi"]
-    mygame = Boggle(grid, dictionary)
-    solution = mygame.getSolution()
-    solution = [x.upper() for x in solution]
-    expected = []
-		solution = sorted(solution)
-    expected = sorted(expected)
-    self.assertEqual(expected, solution)
+    def test_normal_case_3x3(self):
+        grid = [["A", "B", "C"], ["D", "E", "F"], ["G", "H", "I"]]
+        dictionary = ["abc", "abdhi", "abi", "ef", "cfi", "dea"]
+        mygame = Boggle(grid, dictionary)
+        solution = mygame.get_solution()
+        solution = [x.upper() for x in solution]
+        expected = ["abc", "abdhi", "cfi", "dea"]
+        expected = [x.upper() for x in expected]
+        solution = sorted(solution)
+        expected = sorted(expected)
+        self.assertEqual(expected, solution)
 
-class TestSuite_Complete_Coverage(unittest.TestCase):
-  #ADD MANY COMPLEXED TEST CASES
-  def test_case_1(self):  
-    self.assertEqual(True, True)
 
-class TestSuite_Qu_and_St(unittest.TestCase):
-  #ADD QU AND ST TEST CASES
-  def test_case_1(self): 
-    self.assertEqual(True, True)
+class TestSuiteSimpleEdgeCases(unittest.TestCase):
+
+    def test_square_grid_case_1x1(self):
+        grid = [["A"]]
+        dictionary = ["a", "b", "c"]
+        mygame = Boggle(grid, dictionary)
+        solution = mygame.get_solution()
+        solution = [x.upper() for x in solution]
+        expected = []
+        solution = sorted(solution)
+        expected = sorted(expected)
+        self.assertEqual(expected, solution)
+
+    def test_empty_grid_case_0x0(self):
+        grid = [[]]
+        dictionary = ["hello", "there", "general", "kenobi"]
+        mygame = Boggle(grid, dictionary)
+        solution = mygame.get_solution()
+        solution = [x.upper() for x in solution]
+        expected = []
+        solution = sorted(solution)
+        expected = sorted(expected)
+        self.assertEqual(expected, solution)
+
+
+class TestSuiteCompleteCoverage(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual(True, True)
+
+
+class TestSuiteQuAndSt(unittest.TestCase):
+
+    def test_case_1(self):
+        self.assertEqual(True, True)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
